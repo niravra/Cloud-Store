0ab050bff2d1b3e522f0d9dd84d844a9d4f12e45
Niravra Kar <niravrakar1991@gmail.com>
2017-09-27 20:24:20 -0400

    added teammate comments

--- src/main/java/com/csye6225/demo/auth/WebSecurityConfig.java
+++ src/main/java/com/csye6225/demo/auth/WebSecurityConfig.java
@@ -1,5 +1,11 @@
 package com.csye6225.demo.auth;
 
+/**
+ * <Niravra Kar>, <001252638>, <kar.n@husky.neu.edu>
+ * <Sumedh Saraf>, <001267030>, <saraf.s@husky.neu.edu>
+ * <Parakh Mahajan>, <001236045>, <mahajan.pa@husky.neu.edu>
+ * <Ashwini Thaokar>, <001282202>, <thaokar.a@husky.neu.edu>
+ **/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- src/main/java/com/csye6225/demo/dao/PersonDao.java
+++ src/main/java/com/csye6225/demo/dao/PersonDao.java
@@ -1,5 +1,12 @@
 package com.csye6225.demo.dao;
 
+/**
+ * <Niravra Kar>, <001252638>, <kar.n@husky.neu.edu>
+ * <Sumedh Saraf>, <001267030>, <saraf.s@husky.neu.edu>
+ * <Parakh Mahajan>, <001236045>, <mahajan.pa@husky.neu.edu>
+ * <Ashwini Thaokar>, <001282202>, <thaokar.a@husky.neu.edu>
+ **/
+
 import com.csye6225.demo.entity.Person;
 import org.springframework.data.repository.CrudRepository;
 
